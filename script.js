81deb7ffb3840b434847db09fa38c699b461b1f0
8anooceu <jaocarrilhooo2903@gmail.com>
2024-11-18 22:04:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,16 @@ const messages = [
 
 // Seleciona uma mensagem aleatória
 const randomMessage = messages[Math.floor(Math.random() * messages.length)];
-document.getElementById('message').innerText = randomMessage;
+let index = 0;
+
+// Função para simular o efeito de digitação
+function typeEffect() {
+    if (index < randomMessage.length) {
+        document.getElementById('message').innerText += randomMessage.charAt(index);
+        index++;
+        setTimeout(typeEffect, 50); // Ajuste a velocidade da digitação alterando o tempo (em milissegundos)
+    }
+}
+
+// Inicia o efeito de digitação
+typeEffect();
